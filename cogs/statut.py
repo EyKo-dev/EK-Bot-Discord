9a4a0f8cd0ea283685f8f8bfab78da4650b29c1e
Enzo <enzomuller31@gmail.com>
2022-05-29 20:11:41 +0200

    Test

--- cogs/statut.py
+++ cogs/statut.py
@@ -9,4 +9,4 @@ class test(commands.Cog):
     async def on_ready(self):
         await self.bot.change_presence(activity = discord.Streaming(name = "une partie de sudoku...", url = "https://www.twitch.tv/eyko_"))
         print(f"{self.bot.user.name} est connecté au serveur !")
-        print(f"--------------------------------")
+        print(f"-------------------------------")
