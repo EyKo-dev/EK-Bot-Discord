c8674e73b7d0cbdde368a4b39bbcef6832c7a0c8
Enzo <enzomuller31@gmail.com>
2022-05-29 18:43:38 +0200

    Initial commit

---
+++ cogs/statut.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+
+class test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity = discord.Streaming(name = "une partie de sudoku...", url = "https://www.twitch.tv/eyko_"))
+        print(f"{self.bot.user.name} est connecté au serveur !")
+        print(f"--------------------------------")
---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import dotenv
+from discord.ext import commands
+from discord_slash import SlashCommand
+
+from cogs.statut import test
+
+# Recupere le token dans le fichier .env
+dotenv.load_dotenv()
+token = os.getenv('TOKEN')
+
+# Prefix du bot
+bot = commands.Bot(command_prefix = "!", description = "EK Bot")
+slash = SlashCommand(bot, sync_commands = True)
+
+# Commandes
+bot.add_cog(test(bot))
+
+bot.run(token)
