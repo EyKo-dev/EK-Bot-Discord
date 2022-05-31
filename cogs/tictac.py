d3577c271545a58fd2cfc0f3b6be6562dbc036fb
Enzo <enzomuller31@gmail.com>
2022-05-31 06:46:09 +0200

    update

--- cogs/tictac.py
+++ cogs/tictac.py
@@ -7,10 +7,6 @@ class tic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(description="Tic tac... BOOOOOM")
-    async def tic(self, ctx):
-            await ctx.send("tac !")
-
     @cog_ext.cog_slash(name="tic", description="Tic tac... BOOOOOM")
     async def tic(self, ctx):
             await ctx.send("tac !")
